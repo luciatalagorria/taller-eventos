eb41be41f059dcd98c52511c4f3e9565c2b02a1f
Lucía Talagorría <luciatalagorria31@gmail.com>
2025-08-20 17:39:24 -0300

    Solución: on click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,13 @@
 const div = document.getElementById("contenedor");
+const boton = document.getElementById("botonSaludar");
 
+// Evento del div
 div.addEventListener("click", () => {
   alert("Hola! Soy el div");
+});
+
+// Evento del botón con stopPropagation
+boton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("Hola!");
 });
